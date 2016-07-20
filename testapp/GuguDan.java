759c614b958492820ad13972ab22db1692a2013e
CanetRobern <browndeity87@gmail.com>
2016-07-20 12:51:51 +0900

    160720

---
+++ testapp/GuguDan.java
@@ -0,0 +1,28 @@
+class GuguDan{
+
+	public static void main( String [] args){
+
+		for( int i=1 ; i<10 ; i++ ){
+
+			System.out.println(i+"dan");
+			System.out.println();
+			System.out.println("////////////////////////////////////////////////////////////////////////////");
+			System.out.println();
+
+			for( int a=1 ; a<10 ; a++){
+
+				int cpx = a*i;
+
+				System.out.println(i+"x"+a+"="+cpx);
+				
+			}
+
+			System.out.println();
+			System.out.println("////////////////////////////////////////////////////////////////////////////");
+			System.out.println();
+
+		}
+	
+	}
+
+}
---
+++ testapp/MyName.java
@@ -0,0 +1,21 @@
+class MyName{
+
+	//	자바 스크립트와는 달리 변수에 의해 메모리에 올라가는 데이터는
+	//	반드시 그 크기가 명시되어야 한다. ( 자료형을 명시 )
+	String name="Canet";
+	int age=30;					//	메모리에 4byte를 차지
+
+	//	자바와 같이 컴파일 과정이 있는 모든 응용프로그램은 실행하기 위해
+	//	반드시 실행부라 불리는 시작함수가 있어야 한다.
+
+	public static void main( String[] args ){
+
+		for( int a=0 ; a<20 ; a++ ){
+
+			System.out.println("Canet");
+
+		}
+	
+	}
+
+}
